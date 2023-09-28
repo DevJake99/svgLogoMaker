4e788dbe5e5254e55447d7c4eb8df42c93a52b70
DevJake99 <jacobg4@uci.edu>
2023-09-28 00:38:42 -0700

    final version complete

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const {Circle, Square, Triangle} = require("./lib/shapes");
 
-questions = [
+// List of questions for user input via CLI
+var questions = [
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Pick a color for your text using either hexicode or name of a color'
+    },
     {
         type: 'list',
         name: 'shape',
@@ -18,26 +24,83 @@ questions = [
         type: "input",
         name: 'text',
         message:'Enter the text you would like rendered on your image'
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'Pick a color for your text using either hexicode or name of a color'
     }
 ];
 
 
-//  function to write SVG file
-const writeSVG = ({shape, shapeColor, text, textColor}) =>
+// Class with methods to configure the XML for SVG file
+class generateSvg{
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    setTextElement(textColor,text){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
 
-`<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg> `;
+    }
+    render(){
 
-    // inquirer method to implement user input
+        return `
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+        ${this.shapeElement}
+        ${this.textElement}
+        </svg>`
+    }
+    
+};
+
+// Function to write data to file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err)=>{
+        if (err) {
+            console.log(`Error writing file: ${err}` );
+        } else {
+            console.log('File written successfully!');
+        }
+    })
+};
+
+
+ function init() {
+    console.log('Initiating..');
+    // Collect questions from questions variable using inquirer
+    // .then arrow function to collect promise from "promt" method and etract user responses
     inquirer.prompt(questions)
     .then((answers) => {
-        const pageContent = generateReadMe(answers);
-
-        fs.writeFile('README.md', pageContent, (err) =>
-        err ? console.log(err) : console.log('Successfully created README.md!')
-        );
+        // Variable to hold user input for logo text if text is at least 1 character
+        var logoEl = '';
+        if (answers.text.length > 0 ) {
+            logoEl = answers.text
+        } else {
+            console.log('Please Enter at Least One character for logo')
+        } 
+        // Assigning properties of SVG Logo to variables and executing confirmation messages for user
+        var txtColor = answers.textColor;
+        console.log(`User Succesfully Slected ${txtColor} for text color`);
+        var shapeType = answers.shape; 
+        if (shapeType === "Square") {
+            shapeType = new Square();
+            console.log(`User Succesfully Selected ${shapeType} for SVG Shape!`);
+        }else if (shapeType === "Circle") {
+            shapeType = new Circle();
+            console.log(`User Succesfully Selected ${shapeType} for SVG Shape!`);
+        }
+        else if (shapeType === "Triangle") {
+            shapeType = new Triangle();
+            console.log(`User Succesfully Selected ${shapeType} for SVG Shape!`);
+        } 
+        shapeType.setShapeColor(answers.shapeColor);
+        console.log(`User Succesfully selected ${answers.shapeColor} for SVG Shape color!`);
+        //var userShape = answers.shape;
+        var svg = new generateSvg();
+        svg.setTextElement(txtColor, logoEl);
+        svg.setShapeElement(shapeType);
+        svgXML = svg.render();
+        writeToFile('logo.svg', svgXML);
     });
+};
+init()
+
